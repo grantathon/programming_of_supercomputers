3070aa1a1ce74c29d0a593bea631054f82081a0a
Grant <grant.bartel@gmail.com>
2014-10-29 13:47:41 +0100

    Finished txt to bin converter, updated Makefile to use icc compiler

--- 01_assignment/binconv/binconv.c
+++ 01_assignment/binconv/binconv.c
@@ -1,8 +1,11 @@
-// Simple tool that creates a binary file from a dat file
+// Simple tool that converts a text file to a binary file
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define INT_ROW_SIZE 6
+#define DOUBLE_ROW_SIZE 8
 
 int main(int argc, char *argv[])
 {
@@ -13,31 +16,56 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // File variables
+    // File input variables
     FILE *inputFile, *outputFile;
+    int startInners, endInners, startOuters, endOuters;
+    int intInput[INT_ROW_SIZE];
+    double doubleInput[DOUBLE_ROW_SIZE];
 
-    // Open input file
-    printf("Reading input file...\n");
+    // Open text input file
+    printf("Begin converting text file to binary...\n");
     inputFile = fopen(argv[1], "r");
-    printf("Completed reading input file!\n\n");
 
-    // Create copy of binary file in directory of input file
-    printf("Creating output file...\n");
+    // Create binary output file
     outputFile = fopen(argv[2], "wb");
 
-    // Copy input file contents to output file in binary format
-    char element = fgetc(inputFile);
-    while(element != EOF)
+    // Read the initial row parameters that describe file construction
+    fscanf(inputFile, "%d", &startInners);
+    fscanf(inputFile, "%d", &endInners);
+    fscanf(inputFile, "%d", &startOuters);
+    fscanf(inputFile, "%d", &endOuters);
+
+    // Read and write the integer-based rows
+    for(int i = startInners; i < startOuters; i++)
     {
-        fputc(element, outputFile);
-        element = fgetc(inputFile);
+        // Read the row elements
+        for(int j = 0; j < INT_ROW_SIZE; j++)
+        {
+            fscanf(inputFile, "%d", &intInput[j]);
+        }
+
+        // Output the row elements
+        fwrite(intInput, INT_ROW_SIZE, sizeof(*intInput), outputFile);
     }
 
-    printf("Completed creating output file!\n");
+    // Read and write the double-based rows
+    for(int i = startInners; i < startOuters; i++)
+    {
+        // Read the row elements
+        for(int j = 0; j < DOUBLE_ROW_SIZE; j++)
+        {
+            fscanf(inputFile, "%lf", &doubleInput[j]);
+        }
 
-    // Finalize program
+        // Output the row elements
+        fwrite(doubleInput, DOUBLE_ROW_SIZE, sizeof(*doubleInput), outputFile);
+    }
+
+    // Close files
     fclose(inputFile);
     fclose(outputFile);
+
+    printf("Completed converting text file to binary!\n");
 
     return 0;
 }
